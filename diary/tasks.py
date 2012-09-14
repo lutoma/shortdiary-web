9d7139f51bee8d78963395fe15db8d9f868d6c94
Lukas Martini <hello@lutoma.org>
2012-09-13 21:26:49 -0700

    Actually attach post image to mail

--- diary/tasks.py
+++ diary/tasks.py
@@ -4,7 +4,7 @@ import datetime
 from django.utils.translation import ugettext as _
 from celery.schedules import crontab
 from celery.decorators import periodic_task
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from diary.models import Post
 from django.template.loader import get_template
 from django.template import Context
@@ -15,7 +15,7 @@ def process_mails():
 
 	mail_template = get_template('mails/post.txt')
 
-	# Change to years = 0 in production
+	# Change to years = 1 in production
 	searched_date = datetime.date.today() - datetime.timedelta(days = 0)
 
 	posts = Post.objects.filter(date = searched_date, sent = False)
@@ -23,13 +23,16 @@ def process_mails():
 	print('Found {} mail(s) to send'.format(len(posts)))
 
 	for post in posts:
-		print('Sending mail for post {}'.format(post))
+		print('Sending mail for post #{} ({})'.format(post.id, post))
 
-		send_mail(
-			_('[shortdiary] Your post from {0}').format(post.date),
+		mail = EmailMessage(
+			_('Your shortdiary post from {0}').format(post.date),
 			mail_template.render(Context({'post': post})),
-			'shortdiary <team@shortdiary.me>',
-			[post.author.email], fail_silently=False)
+			['shortdiary <team@shortdiary.me>'],
+			[post.author.email])
+
+		mail.attach(post.image.name, post.image.read(), post.image.content_type)
+		mail.send()
 
 		post.sent = True
 		post.save()
