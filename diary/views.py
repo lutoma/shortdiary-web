2b4edef1f1a6b2a7bcc71c5474527f1647e653d9
Lukas Martini <hello@lutoma.org>
2012-09-13 18:07:55 -0700

    Adding index/post/new post/login views

--- diary/views.py
+++ diary/views.py
@@ -1,1 +1,61 @@
-# Create your views here.
+# coding: utf-8
+import datetime
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404, render_to_response
+from django.utils.translation import ugettext as _
+from django.template.context import RequestContext
+from django.contrib.auth.decorators import login_required
+from diary.models import Post
+from django.forms import ModelForm
+
+def index(request):
+	if not request.user.is_authenticated():
+		context = {
+			'title': 'Home',
+			'content': 'Ohai.'
+		}
+		return render_to_response('base.html', context_instance=RequestContext(request, context))
+
+	context = {
+		'title': 'Home',
+		'posts': Post.objects.filter(author = request.user).order_by('-date', '-created_at')[:20],
+	}
+	return render_to_response('index.html', context_instance=RequestContext(request, context))
+
+class PostForm(ModelForm):
+	class Meta:
+		model = Post
+		fields = ('text', 'mood', 'date', 'image')
+
+@login_required
+def new_post(request):
+	if not request.method == 'POST':
+		context = {
+			'title': 'New post',
+			'today': datetime.date.today(),
+			'yesterday': datetime.date.today() - datetime.timedelta(days=1),
+			'form': PostForm()
+		}
+
+		return render_to_response('new_post.html', context_instance=RequestContext(request, context))
+
+	# Request method is POST
+	form = PostForm(request.POST, request.FILES)
+	if not form.is_valid():
+		return HttpResponse('Invalid data.')
+
+	post = form.save(commit = False)
+	post.author = request.user
+	post.save()
+
+	return HttpResponseRedirect('/')
+
+@login_required
+def show_post(request, post_id):
+	post = get_object_or_404(Post, id = post_id, author = request.user)
+
+	context = {
+		'title': 'Post from {}'.format(post.date),
+		'post': post,
+	}
+	return render_to_response('show_post.html', context_instance=RequestContext(request, context))
--- shortdiary/urls.py
+++ shortdiary/urls.py
@@ -1,13 +1,28 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+if settings.DEBUG:
+	urlpatterns = patterns('django.views.static',
+		url(r'^static/(?P<path>.*)$',  'serve', {'document_root': '/tmp/shortdiary-static/', 'show_indexes': True }),
+		url(r'^asset/(?P<path>.*)$',  'serve', {'document_root': '/tmp/shortdiary-asset/', 'show_indexes': True }),
+	)
+else:
+	urlpatterns = patterns('')
+
+urlpatterns += patterns('',
 	# Examples:
 	# url(r'^$', 'shortdiary.views.home', name='home'),
 	# url(r'^shortdiary/', include('shortdiary.foo.urls')),
 
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
+
+	url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+	url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+
+	url(r'^post/new/', 'diary.views.new_post'),
+	url(r'^post/show/(?P<post_id>[0-9]+)/', 'diary.views.show_post'),
+	url(r'^/?$', 'diary.views.index'),
 )
