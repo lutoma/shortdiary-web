7cb948174ec0f3c6dff4c49a184837ee87c3ef17
Lukas Martini <hello@lutoma.org>
2012-10-02 23:08:03 -0700

    Fixing small bug in settings.py

--- shortdiary/settings.py
+++ shortdiary/settings.py
@@ -100,7 +100,7 @@ TEMPLATE_CONTEXT_PROCESSORS = ('django.contrib.auth.context_processors.auth',
 	'django.core.context_processors.static',
 	'django.core.context_processors.tz',
 	'django.contrib.messages.context_processors.messages',
-	debug_context,
+	'shortdiary.settings.debug_context',
 )
 
 ROOT_URLCONF = 'shortdiary.urls'
