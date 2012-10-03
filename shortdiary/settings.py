1a93e2e34f9aed1b17b375e38f78b6aa6bae91fe
Lukas Martini <hello@lutoma.org>
2012-10-02 22:56:28 -0700

    Add piwik tracking code in production

--- shortdiary/settings.py
+++ shortdiary/settings.py
@@ -91,6 +91,18 @@ MIDDLEWARE_CLASSES = (
 	# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+debug_context = lambda request: {'DEBUG': DEBUG}
+
+TEMPLATE_CONTEXT_PROCESSORS = ('django.contrib.auth.context_processors.auth',
+	'django.core.context_processors.debug',
+	'django.core.context_processors.i18n',
+	'django.core.context_processors.media',
+	'django.core.context_processors.static',
+	'django.core.context_processors.tz',
+	'django.contrib.messages.context_processors.messages',
+	debug_context,
+)
+
 ROOT_URLCONF = 'shortdiary.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
