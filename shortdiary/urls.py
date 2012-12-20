228c3f88716145a6855845ff653f81a12c50e1fe
Lukas Martini <hello@lutoma.org>
2012-12-20 06:10:12 +0100

    Shortening post URLs

--- shortdiary/urls.py
+++ shortdiary/urls.py
@@ -30,6 +30,6 @@ urlpatterns += patterns('',
 	url(r'^tos/', 'diary.views.tos'),
 
 	url(r'^post/new/', 'diary.views.new_post'),
-	url(r'^post/show/(?P<post_id>[0-9]+)/', 'diary.views.show_post'),
+	url(r'^post/(?P<post_id>[0-9]+)/', 'diary.views.show_post'),
 	url(r'^/?$', 'diary.views.index'),
 )
