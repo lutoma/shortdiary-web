dff9d0a1b4dd853bece48541e489c5537a8cfa8e
Lukas Martini <hello@lutoma.org>
2014-01-06 04:55:13 +0100

    Pretty new menu that shows the current page

--- shortdiary/settings.py
+++ shortdiary/settings.py
@@ -103,7 +103,9 @@ MIDDLEWARE_CLASSES = (
 
 debug_context = lambda request: {'DEBUG': DEBUG}
 
-TEMPLATE_CONTEXT_PROCESSORS = ('django.contrib.auth.context_processors.auth',
+TEMPLATE_CONTEXT_PROCESSORS = (
+	'django.contrib.auth.context_processors.auth',
+	'django.core.context_processors.request',
 	'django.core.context_processors.debug',
 	'django.core.context_processors.i18n',
 	'django.core.context_processors.media',
