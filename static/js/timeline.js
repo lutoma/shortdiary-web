79e6eeae89eb0dfc642045636a321c3512c6d9d0
Lukas Martini <hello@lutoma.org>
2014-09-12 01:47:26 +0200

    Dashboard timeline: Correctly sort and render timeline

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -2,8 +2,32 @@
 
 function render(posts) {
 	console.log(posts)
-	var monthNames = [ "January", "February", "March", "April", "May", "June",
-    	"July", "August", "September", "October", "November", "December" ];
+
+	// TODO Localization
+	// FIXME Why is this an object?
+	var month_names = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
+    	7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"};
+
+    // Reformat and sort posts (objects are unsorted, so convert to arrays)
+    posts = _.map(posts, function (year_posts, year) {
+    	year_posts = _.map(year_posts, function (month_posts, month) {
+    		return {
+    			'year': year,
+    			'month': month, 
+    			'month_name': month_names[month],
+    			'posts': month_posts
+    		};
+    	});
+
+    	year_posts = _.sortBy(year_posts, function(month) { return -month.month; });
+    	console.log('sorted posts: ', year_posts)
+
+    	return {'year': year, 'posts': year_posts};
+    });
+
+    posts = _.sortBy(posts, function(year) { return -year.year; });
+
+	console.log(posts)
 
 	var template = Handlebars.compile($("#timeline-main").html());
 	var html = template({timegroups: posts});
@@ -20,5 +44,5 @@ function render(posts) {
 $(window).ready(function() {
 	Handlebars.registerPartial("timeline-boxlist", $("#timeline-boxlist-partial").html());
 
-	$.get('/api/v1/posts/timeline/', render);
+	$.get('/api/v1/posts/timeline/?format=json', render);
 });
