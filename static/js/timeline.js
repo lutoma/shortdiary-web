ea9b9350d4afe5cac12a882409aa1cc5a68e924b
Lukas Martini <hello@lutoma.org>
2014-09-12 01:54:11 +0200

    Indendation fix

--- static/js/timeline.js
+++ static/js/timeline.js
@@ -6,26 +6,26 @@ function render(posts) {
 	// TODO Localization
 	// FIXME Why is this an object?
 	var month_names = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
-    	7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"};
+		7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"};
 
-    // Reformat and sort posts (objects are unsorted, so convert to arrays)
-    posts = _.map(posts, function (year_posts, year) {
-    	year_posts = _.map(year_posts, function (month_posts, month) {
-    		return {
-    			'year': year,
-    			'month': month, 
-    			'month_name': month_names[month],
-    			'posts': month_posts
-    		};
-    	});
+	// Reformat and sort posts (objects are unsorted, so convert to arrays)
+	posts = _.map(posts, function (year_posts, year) {
+		year_posts = _.map(year_posts, function (month_posts, month) {
+			return {
+				'year': year,
+				'month': month, 
+				'month_name': month_names[month],
+				'posts': month_posts
+			};
+		});
 
-    	year_posts = _.sortBy(year_posts, function(month) { return -month.month; });
-    	console.log('sorted posts: ', year_posts)
+		year_posts = _.sortBy(year_posts, function(month) { return -month.month; });
+		console.log('sorted posts: ', year_posts)
 
-    	return {'year': year, 'posts': year_posts};
-    });
+		return {'year': year, 'posts': year_posts};
+	});
 
-    posts = _.sortBy(posts, function(year) { return -year.year; });
+	posts = _.sortBy(posts, function(year) { return -year.year; });
 
 	console.log(posts)
 
