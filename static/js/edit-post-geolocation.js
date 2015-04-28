b20e47d26353512aca3aa35a711953338358d557
Lukas Martini <hello@lutoma.org>
2015-04-28 07:15:02 +0200

    Edit post: Allow manually setting a post location

--- static/js/edit-post-geolocation.js
+++ static/js/edit-post-geolocation.js
@@ -1,16 +1,5 @@
-function location_callback(position) {
-	$('input[name=location_lat]').val(position.coords.latitude.toFixed(12));
-	$('input[name=location_lon]').val(position.coords.longitude.toFixed(12));
-
-	L.mapbox.accessToken = 'pk.eyJ1IjoibHV0b21hIiwiYSI6ImVkbzF4MG8ifQ.pIpC2pu9savl1ZZLl8TGrA';
-	var map = L.mapbox.map('map-canvas', 'lutoma.m1iha18e', {zoomControl: false});
-	map.scrollWheelZoom.disable();
-	var layer = L.mapbox.featureLayer().addTo(map);
-
-	map.setView([position.coords.latitude, position.coords.longitude], 16);
-	L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
-
-	var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+function geocode_location(latitude, longitude) {
+	var pos = new google.maps.LatLng(latitude, longitude);
 
 	geocoder = new google.maps.Geocoder();
 
@@ -29,6 +18,30 @@ function location_callback(position) {
 			$('input[name=location_verbose]').val(value['formatted_address'])
 		});
 	});
+}
+
+function location_callback(position) {
+	$('input[name=location_lat]').val(position.coords.latitude.toFixed(12));
+	$('input[name=location_lon]').val(position.coords.longitude.toFixed(12));
+
+	L.mapbox.accessToken = 'pk.eyJ1IjoibHV0b21hIiwiYSI6ImVkbzF4MG8ifQ.pIpC2pu9savl1ZZLl8TGrA';
+	var map = L.mapbox.map('map-canvas', 'lutoma.m1iha18e');
+	var layer = L.mapbox.featureLayer().addTo(map);
+
+	map.setView([position.coords.latitude, position.coords.longitude], 16);
+	marker = L.marker([position.coords.latitude, position.coords.longitude], {draggable: true}).addTo(map);
+
+	marker.on('dragend', function(e){
+		var latitude = e.target._latlng['lat'];
+		var longitude = e.target._latlng['lng'];
+
+		$('input[name=location_lat]').val(latitude.toFixed(12));
+		$('input[name=location_lon]').val(longitude.toFixed(12));
+
+		geocode_location(latitude, longitude);
+	});
+
+	geocode_location(position.coords.latitude, position.coords.longitude);
 }
 
 $(document).ready(function() {
