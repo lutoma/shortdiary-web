710bc4b572f376e1dda2f42066aa20904e5eab36
Lukas Martini <hello@lutoma.org>
2021-03-26 18:01:00 +0100

    Docker support files

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM python:2.7-buster
+LABEL maintainer="lukas@fnordserver.eu"
+ENV PYTHONUNBUFFERED 1
+
+WORKDIR /code
+COPY . /code/
+
+RUN pip install --no-cache-dir poetry
+RUN poetry install
+RUN poetry add gunicorn
+RUN poetry run ./manage.py collectstatic --noinput
+
+RUN mkdir /data
+RUN ln -s /data/settings_local.py shortdiary/local_settings.py
+RUN ln -s /data/asset asset
+
+RUN wget "https://caddyserver.com/api/download?os=linux&arch=amd64" -O /usr/local/bin/caddy
+RUN chmod +x /usr/local/bin/caddy
+
+RUN apt update && apt install -y supervisor
+CMD ["/usr/bin/supervisord", "-c", "/code/supervisord.conf"]
--- shortdiary/settings.py
+++ shortdiary/settings.py
@@ -50,7 +50,7 @@ LANGUAGES = (
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/tmp/shortdiary-asset/'
+MEDIA_ROOT = os.getcwd() + '/asset/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -61,7 +61,7 @@ MEDIA_URL = '/asset/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/tmp/shortdiary-static/'
+STATIC_ROOT = os.getcwd() + '/static-collect/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
