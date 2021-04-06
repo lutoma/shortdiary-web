f2f134552248bb63d4388d335440fa1becfa65c3
Lukas Martini <hello@lutoma.org>
2021-04-06 11:15:09 +0200

    Move assets/media to object storage

--- shortdiary/settings_local.example.py
+++ shortdiary/settings_local.example.py
@@ -60,3 +60,13 @@ EMAIL_USE_TLS = True
 TWILIO_ACCOUNT_SID = ''
 TWILIO_AUTH_TOKEN = ''
 TWILIO_CALLER_ID = ''
+
+# Media storage
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = 'shortdiary-asset'
+AWS_QUERYSTRING_AUTH = True
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_ENDPOINT_URL = 'https://s3.dbl.cloud.syseleven.net'
+AWS_S3_REGION_NAME = 'dbl'
