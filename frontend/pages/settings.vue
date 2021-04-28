aafcb30ba0048d7594f67472cbc6bc53a4b1b654
Lukas Martini <hello@lutoma.org>
2021-04-28 05:43:45 +0200

    frontend: Make settings page functional

--- frontend/pages/settings.vue
+++ frontend/pages/settings.vue
@@ -5,89 +5,54 @@
 		<el-row :gutter="50">
 			<el-col :span="12">
 				<el-card>
-					<h2>Account settings</h2>
-					<el-form ref="form" :model="form" label-width="160px">
+					<h2>Account</h2>
+					<el-form ref="form" :model="user" label-position="left" label-width="200px">
 						<el-form-item label="Username">
 							<el-input v-model="user.username" />
 						</el-form-item>
 						<el-form-item label="Email">
 							<el-input v-model="user.email" />
 						</el-form-item>
-						<el-form-item label="New password">
-							<el-input v-model="form.password" show-password />
-						</el-form-item>
-						<el-form-item label="New password (repeat)">
-							<el-input v-model="form.password_repeat" show-password />
-						</el-form-item>
+						<p><n-link to="/change-password">Change password</n-link></p>
 					</el-form>
 				</el-card>
 			</el-col>
 
 			<el-col :span="12">
 				<el-card>
-					<h2>Notification settings</h2>
-
-					<el-form ref="form" :model="form" label-width="160px">
-						<el-form-item label="Email notifications">
-							<el-checkbox-group v-model="form.type">
-								<el-checkbox label="Streak reminders" name="type" />
-								<el-checkbox label="Past memories" name="type" />
-							</el-checkbox-group>
+					<h2>Privacy</h2>
+					<el-form ref="form" :model="user" label-position="left" label-width="200px">
+						<el-form-item label="Add location to posts">
+							<el-switch v-model="user.geolocation_enabled" />
 						</el-form-item>
-					</el-form>
-				</el-card>
-			</el-col>
 
-			<el-col :span="12">
-				<el-card>
-					<h2>Post settings</h2>
-					<el-form ref="form" :model="form" label-width="160px">
-						<el-form-item label="Default post visibility">
-							<el-select v-model="form.region" placeholder="please select your zone">
-								<el-option label="Zone one" value="shanghai"></el-option>
-								<el-option label="Zone two" value="beijing"></el-option>
-							</el-select>
-						</el-form-item>
-						<el-form-item label="Include locations">
-							<el-switch v-model="form.delivery"></el-switch>
+						<el-form-item label="Include me in leaderboard">
+							<el-switch v-model="user.include_in_leaderboard" />
 						</el-form-item>
 					</el-form>
 				</el-card>
 			</el-col>
 		</el-row>
 
-		<el-button type="primary" @click="onSubmit">Save settings</el-button>
+		<el-button type="primary" @click="saveSettings">Save settings</el-button>
 	</div>
 </template>
 
-
 <script>
-	export default {
-		data() {
-			let data = {
-				form: {
-					name: '',
-					region: '',
-					date1: '',
-					date2: '',
-					delivery: false,
-					type: [],
-					resource: '',
-					desc: '',
-					password: '',
-					password_repeat: '',
-				}
-			}
-
-			data['user'] = this.$auth.user
-			return data
-		},
-		methods: {
-			onSubmit() {
-				console.log('submit!');
-			}
+export default {
+	data() {
+		return {
+			user: JSON.parse(JSON.stringify(this.$auth.user))
+		}
+	},
+	methods: {
+		async saveSettings() {
+			// FIXME error handling
+			await this.$axios.$patch('/user/', this.user)
+			this.$message({ type: 'success', message: 'Your settings have been saved.' })
 		}
 	}
+}
 </script>
 
 <style>
