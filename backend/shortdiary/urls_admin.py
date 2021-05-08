8cc4e71a89c530a1f1f9151050b2fc66c6b73cca
Lukas Martini <hello@lutoma.org>
2021-05-08 13:06:47 +0200

    backend: Fix admin static files serving

--- backend/shortdiary/urls_admin.py
+++ backend/shortdiary/urls_admin.py
@@ -3,11 +3,10 @@
 
 from django.urls import path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib import admin
 
 urlpatterns = [
-	path('', admin.site.urls),
+	path('static/<path:path>', serve, {'document_root': settings.STATIC_ROOT}),
+	path('', admin.site.urls)
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
