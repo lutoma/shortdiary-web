5406f5e87069ec1929f83a6c0ef58e53e2d35194
Lukas Martini <hello@lutoma.org>
2022-06-20 23:37:39 +0200

    Add rollup visualizer

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import vue from '@vitejs/plugin-vue';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -33,6 +34,15 @@ export default defineConfig({
 			},
 			'/avatar/': 'https://www.gravatar.com/',
 			'/geocode/': 'https://api.opencagedata.com',
+		},
+	},
+
+	build: {
+		sourcemap: true,
+		rollupOptions: {
+			plugins: [
+				visualizer(),
+			],
 		},
 	},
 });
